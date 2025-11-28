65767520b9acdae11afd87d05cf622218a3080f6
Henrique Silva <143834219+Henrique-nte@users.noreply.github.com>
2025-11-27 22:02:09 -0300

    Uo

--- index.c
+++ index.c
@@ -5,9 +5,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef struct
+{
+    char nome[50];
+    int idade;
+} Contato;
+
 // FUNÇÃO PRINCIPAL
 int main(void)
 {
+    Contato array[3];
+
+    printf("--------MENU-------");
+
+
+    strcpy(array[0].nome, "Henrique");
+    array[0].idade = 18;
     printf("Hellor World!");
     return 0;
 }
